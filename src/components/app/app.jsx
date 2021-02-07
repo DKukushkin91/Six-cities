308a003bfa267d401273ed29c9f6b265b76c223f
DSKukushkin91 <Jame7on@gmail.com>
2021-02-07 20:59:09 +0500

    4. Маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MainPage from '../main-page/main-page';
+import FavoritesScreen from '../favorites/favorites';
+import LoginScreen from '../login/login';
+import OfferPropertyScreen from '../offer-property/offer-property';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 
-const App = (props) => {
-  const {cardsCount} = props;
+const App = ({cardsCount}) => {
   return (
-    <MainPage cardsCount={cardsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage cardsCount={cardsCount} />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+        <Route exact path="/login">
+          <LoginScreen />
+        </Route>
+        <Route exact path="/offer/:id">
+          <OfferPropertyScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
