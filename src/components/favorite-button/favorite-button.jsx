72f7bca55ed362dfc6dfc264f8f20a7a1896fc8a
DSKukushkin91 <Jame7on@gmail.com>
2021-03-20 18:52:05 +0500

    remove useCallback

--- src/components/favorite-button/favorite-button.jsx
+++ src/components/favorite-button/favorite-button.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {favoriteStatus} from '../../store/api-actions';
 import {useDispatch, useSelector} from "react-redux";
